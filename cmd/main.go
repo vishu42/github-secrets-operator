23aff6e74da5ec2e4d5da06506f2f3ade0db49b6
vishu42 <tewatiavishal10@gmail.com>
2024-09-19 15:18:21 +0530

    rename api

--- cmd/main.go
+++ cmd/main.go
@@ -144,11 +144,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.GithubSecretReconciler{
+	if err = (&controller.SecretSyncReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "GithubSecret")
+		setupLog.Error(err, "unable to create controller", "controller", "SecretSync")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
